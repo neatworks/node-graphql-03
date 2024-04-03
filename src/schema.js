4adc546eb74601bc3e5a88b52d3cd90a52e9c4a8
Shima Drozz <shimadrozz@gmail.com>
2024-04-03 03:36:17 -0400

    Initial commit

---
+++ src/schema.js
@@ -0,0 +1,27 @@
+//* node-graphql/src/schema.js
+
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Student {
+    id: ID!
+    email: String!
+    fullName: String!
+    dept: String
+    enrolled: Boolean
+  }
+
+  type Query {
+    enrollment: [Student!]
+    students: [Student!]!
+    student(id: ID!): Student
+  }
+
+  type Mutation {
+    registerStudent(email: String!, fullName: String!, dept: String): Student!
+    enroll(id: ID!): Student
+  }
+`;
+module.exports = {
+  typeDefs,
+};
